d15cf24b448d3bfcf5680097aa7512c52045cd68
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2023-09-09 17:27:17 +0100

    finishing and testing

--- Wpf-Navigation/MainWindow.xaml.cs
+++ Wpf-Navigation/MainWindow.xaml.cs
@@ -15,14 +15,22 @@ using System.Windows.Shapes;
 
 namespace Wpf_Navigation
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void sidebar_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            var selected = sidebar.SelectedItem as NavButton;
+
+            navframe.Navigate(selected.Navlink);
+
+        }
+
+        
     }
 }
